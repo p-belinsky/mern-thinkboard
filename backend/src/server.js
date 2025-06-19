2880f16b003c942130e36d15ba73f854813fe1a6
Phil Belinsky <philbelinsky@yahoo.com>
2025-06-19 07:28:25 -0400

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,22 +6,34 @@ import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+if(process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  }))
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 app.use(cookieParser());
 
 app.use("/api/notes", notesRoute);
 app.use("/api/auth", authRoute)
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
