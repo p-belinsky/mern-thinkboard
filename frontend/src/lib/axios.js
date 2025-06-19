dcc9d8468ad43bef49ad95fed6ce40c5c6e6b4ff
Phil Belinsky <philbelinsky@yahoo.com>
2025-06-19 13:01:41 -0400

    added backend render link

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = import.meta.env.NODE_ENV === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.NODE_ENV === "development" ? "http://localhost:5001/api" : "https://mern-thinkboard-c6bd.onrender.com/api";
 const api = axios.create({
     baseURL: BASE_URL
 });
