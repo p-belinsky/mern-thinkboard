786c63535cd0dd6558f14ce3518954c68d2733d3
Phil Belinsky <philbelinsky@yahoo.com>
2025-06-19 13:43:58 -0400

    changed api url

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/api/auth";
+const API_URL = import.meta.env.VITE_API_URL || "/api/auth";
 
 axios.defaults.withCredentials = true;
 
